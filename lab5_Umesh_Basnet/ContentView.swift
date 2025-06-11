6fd9842c839df3bffcf75166f9acc9a3fdd28aac
Umesh Basnet <basnet0umesh@gmail.com>
2025-06-11 13:43:56 -0400

    -[x] initial setup

---
+++ lab5_Umesh_Basnet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lab5_Umesh_Basnet
+//
+//  Created by Umesh Basnet on 11/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lab5_Umesh_Basnet/lab5_Umesh_BasnetApp.swift
@@ -0,0 +1,17 @@
+//
+//  lab5_Umesh_BasnetApp.swift
+//  lab5_Umesh_Basnet
+//
+//  Created by Umesh Basnet on 11/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct lab5_Umesh_BasnetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
